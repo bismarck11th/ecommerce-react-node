97d6387235023b42d67045ae8ac5040180447045
kazuki827 <xm.kazuki827@gmail.com>
2021-11-05 12:46:46 +0900

    create nodejs server

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'Nick Slim Shirts',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 200,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product'
+    },
+    {
+      _id: '2',
+      name: 'Nick Slim Shirts',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 200,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product'
+    },
+    {
+      _id: '3',
+      name: 'Nick Slim Shirts',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 200,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product'
+    },
+    {
+      _id: '4',
+      name: 'Nick Slim Shirts',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 200,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product'
+    },
+    {
+      _id: '5',
+      name: 'Nick Slim Shirts',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 200,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product'
+    },
+    {
+      _id: '6',
+      name: 'Nick Slim Shirts',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 200,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product'
+    }
+  ]
+};
+
+export default data;
